d62829ba8624b9263d23b2564fa3cdec571bce48
kde66034 <kde66034@gmail.com>
2022-11-29 17:09:12 +0900

    221129

--- src/components/WeatherLeft.jsx
+++ src/components/WeatherLeft.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import clearIcon from '../assets/img/weather/icon/Sunny-Bold.svg'
 import cloudyIcon from '../assets/img/weather/icon/PartlyCloudy-Bold.svg'
 import rainyIcon from '../assets/img/weather/icon/Rainy-Bold.svg'
 import snowyIcon from '../assets/img/weather/icon/Snowy-Bold.svg'
 import rainthunderIcon from '../assets/img/weather/icon/RainThunder-Bold.svg'
-import { fetchAPI } from '../utils/fetchAPI';
+
+import { fetchAPI } from '../utils/fetchAPI'
+import { useParams, Link } from 'react-router-dom'
 
 const WeatherLeft = () => {
   return (
     <div className="weather__left">
         <h2 className="weather_title">WORLD WEATHER</h2>
         <div className="weather__bottom">
-            <div className="weather_temp">-1˚</div>
+            <div className="weather_temp">
+              -1˚
+            </div>
             <div className="weather__region">
                 Seoul
                 <p className="weather_time">12:12 AM / 2022-11-24</p>
